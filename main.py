5325459b7c1d9b41065c8456c3197695d23979a6
Huu Tri Huynh <134994926+hhtri@users.noreply.github.com>
2025-09-14 22:24:15 -0500

    assignment complete

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ See assignment-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
-    pass
+    if x <= 1:
+        return x
+    ra = foo(x - 1)
+    rb = foo(x - 2)
+    return ra + rb
 
 def longest_run(mylist, key):
-    ### TODO
-    pass
+    best = 0
+    cur = 0
+    for v in mylist:
+        if v == key:
+            cur += 1
+            if cur > best:
+                best = cur
+        else:
+            cur = 0
+    return best
 
 
 class Result:
@@ -37,8 +48,33 @@ def to_value(v):
         return int(v)
         
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
+    n = len(mylist)
+    if n == 0:
+        return Result(0, 0, 0, True)
+    if n == 1:
+        if mylist[0] == key:
+            return Result(1, 1, 1, True)
+        else:
+            return Result(0, 0, 0, False)
+
+    mid = n // 2
+    left_res  = longest_run_recursive(mylist[:mid], key)
+    right_res = longest_run_recursive(mylist[mid:], key)
+
+    # entire range is all key only if both halves are all key
+    is_entire = left_res.is_entire_range and right_res.is_entire_range
+
+    # prefix run can extend from left half into right half if left half is all key
+    left_size = left_res.left_size + right_res.left_size if left_res.is_entire_range else left_res.left_size
+
+    # suffix run can extend from right half into left half if right half is all key
+    right_size = right_res.right_size + left_res.right_size if right_res.is_entire_range else right_res.right_size
+
+    # best run is in left, in right, or crossing the middle
+    cross = left_res.right_size + right_res.left_size
+    longest_size = max(left_res.longest_size, right_res.longest_size, cross)
+
+    return Result(left_size, right_size, longest_size, is_entire)
 
 
 
